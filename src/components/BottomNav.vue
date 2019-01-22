2baf0ba58c61cb111624ff531cf7138906c165fa
Abe Perl <abeperl@gmail.com>
2019-01-22 15:21:03 -0500

    Init Commit

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <v-bottom-nav app fixed :value="true">
+
+    <v-btn flat color="primary" :to="{ path: '/'}">
+      <span>Top Stories</span>
+      <v-icon>thumb_up</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" :to="{ name: 'code-examples'}">
+      <span>Code Examples</span>
+      <v-icon>code</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" :to="{ name: 'my-favorites'}">
+      <span>Favorites</span>
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
+  </v-bottom-nav>
+
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  @Component
+  export default class BottomNav extends Vue {
+  }
+</script>
